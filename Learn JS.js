97c7d76b91d76ccbfc9d14e38929d7ab64501d90
Youssef <yr20032003@gmail.com>
2024-07-15 20:48:32 +0300

    JS1

--- Learn JS.js
+++ Learn JS.js
@@ -1,3 +1,19 @@
-console.log("DevJoe");
-console.log("Youssef Ragab");
-console.log("TEST")
+console.log(" Made by DevJoe");
+
+//variables
+//1. var
+//2. let
+//3. const
+
+
+//example of variable
+const foo =1;
+let bar = foo;
+bar = 9;
+console.log(foo, bar)
+
+
+const birthYear = 2003; //fixed
+let currentYear = 2024; //can change
+let age = currentYear - birthYear;
+console.log(age)
