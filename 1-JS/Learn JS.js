91da063e74462e32f527e8223cd40c96f1a6aa1a
Youssefragab26 <youssef26ragab@gmail.com>
2024-08-11 14:56:21 +0300

    JOE

---
+++ 1-JS/Learn JS.js
@@ -0,0 +1,41 @@
+document.write('Hello in JS') // write in page
+console.log('hello in JS') // write in console
+window.alert('Hello in my JS lesrn page') // write in popup
+
+//(example)                                  //(The result)
+document.write('<h1>Hello World in JS (h1)</h1>') //hello world
+//window.alert('<h1>hello world</h1>') //<h1>hello world</h1>
+console.log('<h1>hello world</h1>') //<h1>hello world</h1>
+////                                                                               
+//Data types
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+////                                                                               
+//Variables
+var username;
+username = "Joe";
+console.log(username)
+let name ;
+name = "Joe2";
+console.log(name)
+const name2 = "Joe3";
+console.log(name2)
+////                                                                               
---
+++ 1-JS/main.js
@@ -0,0 +1,11 @@
+// window.alert('Hello in my JS lesrn page')
+
+let name = "Youssef Ragab";
+let age = 21;
+let username = "joe_ragab26";
+window.alert("صافى الداخل بالجنيه المصرى")
+// document.write(`Hello my name is ${name} and my age is ${age} and my username is ${username}`)
+let zakah = window.prompt();
+// document.write(0.025 * zakah + " EGP")
+document.write(`Your Zakah is ${0.025 * zakah + " EGP"}`)
+
