5cca9a3695663e86b1ad1879532111fa1bd92757
Youssefragab26 <youssef26ragab@gmail.com>
2024-08-12 19:48:44 +0300

    YR

---
+++ 4-Python/function2.py
@@ -0,0 +1,13 @@
+def add1(num1, num2):
+    print(num1 + num2)
+def add2(num1, num2):
+    print(num1 - num2)
+def addNum1(num1, num2):
+    print(num1 * num2)
+def addNum2(num1, num2):
+    print(num1 / num2)
+
+add1(10 , 5)
+add2(5 , 5)
+addNum1(10 , 5)
+addNum2(10 , 5)
---
+++ 4-Python/module.py
@@ -0,0 +1,4 @@
+#import on or two functions from module
+from random import randint, random
+print(f"print random integer {randint(100, 900)}")
+print(f"print random folat {random()}")
