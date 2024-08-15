2f4475f9ecb0a2c839e16c182d839ff8e38e32c9
Youssefragab26 <youssef26ragab@gmail.com>
2024-08-15 19:55:19 +0300

    YR

--- 1-JS/Learn JS.js
+++ 1-JS/Learn JS.js
@@ -40,6 +40,64 @@ const name2 = "Joe3";
 console.log(name2)
 ////                                                                               
 //Array
-let beyou = ['joe','nour', 'ali' , 'halol']
-//             0      1       2       3
-console.log(beyou(0));
+// let beyou = ['joe','nour', 'ali' , 'halol']
+// //             0      1       2       3
+// console.log(beyou(0));
+////                                                                                
+console.log(name.toUpperCase());  //toUpperCase() is a method that converts the string to uppercase
+console.log(name.toLowerCase());  //toLowerCase() is a method that converts the string to lowercase
+console.log(name.trim());  //trim() is a method that removes the whitespace from the string
+// console.log(name.length()) // length() is a method that returns the length of the string
+////                                                                                
+//String methods
+console.log(name.slice(0, 2)); // slice() is a method that returns a part of the string
+console.log(name.slice(0, 2)); // slice() is a method that returns a part of the string
+////                                                                                
+//  && and
+//  || or
+//  ! not
+// == equal
+// != not equal
+// === equal value and type
+// !== not equal value and type
+// > greater than
+// < less than
+// >= greater than or equal to
+// <= less than or equal to 
+////                                                                                
+//if else
+        let num1 = prompt("num1");
+        let num2 = prompt("num2");
+        if(num1+num2 == 10)
+        {
+            document.write("<h1>True</h1>");
+        }
+        else if(num1+num2 !== 10) {
+            document.write("<h1>False</h1>");
+        }
+
+
+// if    اول شرط
+// else if    لو اول شرط غلط يرجع الثاني
+// else    لو الشرطين غلط يرجع الثالث
+////                                                                                
+//switch
+let day = prompt("Enter a number between 1 and 7");
+switch (day) {
+    case "joe":
+        console.log("Monday");
+        break;
+    case "joe2":
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("Not a valid day");
+}
+////                                                                                
+//Loops
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+
