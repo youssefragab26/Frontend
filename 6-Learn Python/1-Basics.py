721b3687f2c16d0226f81c8d89a25c2e5076736f
Youssefragab26 <youssef26ragab@gmail.com>
2024-08-30 18:46:12 +0300

    All

--- 6-Learn Python/1-Basics.py
+++ 6-Learn Python/1-Basics.py
@@ -1,5 +1,21 @@
 # Basics
+#-----------------------------
 print("Hello World")
 print("My first python code ")
 print("Iam DevJoe")
 #------------------------------
+#Variable
+N = 300
+print(N)
+Y = 500
+print(N + Y)
+A = B = C = 250
+print(A, B, C )
+#------------------------------
+#Variable Types
+var = 1250
+print(var)
+
+name = "joe"
+age = 21
+print(name , age)
