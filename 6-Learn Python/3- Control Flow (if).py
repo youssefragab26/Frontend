ae2a6e415cb22e0c2306187de025b54484bee6be
Youssefragab26 <youssef26ragab@gmail.com>
2024-09-11 13:28:58 +0300

    PY

--- 6-Learn Python/3- Control Flow (if).py
+++ 6-Learn Python/3- Control Flow (if).py
@@ -7,3 +7,35 @@ elif x == 0 :
 else:
     print("Number is Negative")
 
+#equals: a == b
+#not equals: a != b
+#less than: a < b
+#greater than: a > b
+
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+print("A") if a < b else print("B")
+
+
+n = 330
+y = 330
+print("N") if n > y else print("=") if n == y else print("Y")
+
+s = 200
+r = 33
+k = 500
+#And
+if s > r and k > s:
+    print("Both conditions are true")
+
+#Or
+if s > r or s > k:
+    print("At least one of the conditions is True")
+
+#Not
+if not a > b :
+    print("a is NOT greater than b")
+
---
+++ 6-Learn Python/4-Loops.py
@@ -0,0 +1,12 @@
+# While Loops
+i = 1
+while i < 9:
+    print(i)
+    if i == 3:
+        break
+    i+= 1
+
+# For Loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
