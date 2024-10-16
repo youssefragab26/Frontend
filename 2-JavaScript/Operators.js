cf367f7f023b1909910b1b5c03bd845a204c9f93
Youssefragab26 <youssef26ragab@gmail.com>
2024-10-16 17:32:37 +0300

    update

--- 2-JavaScript/Operators.js
+++ 2-JavaScript/Operators.js
@@ -1,30 +1,35 @@
-/* Arithmetic Operators in JS
-    + addition
-    - subtraction
-    * multiplication
-    / division
-    % modulus
-    ++ increment
-    -- decrement
-*/
+// Logical AND operator: && 
 
-/* Comparsion Operators in JS:
-    == equal to
-    != not equal to
-    > greater than
-    < less than
-    >= greater than or equal to
-    <= less than or equal to
-*/
+// The logical AND operator is, for example, used to confirm if multiple comparisons will return true.
+// The logical AND operator returns true if both the operands are true.
+// In JavaScript, this operator consists of two ampersand symbols together: &&.
 
-console.log(2 + 2)
-console.log(1 + 2 + 3 + 4 + 5)
-console.log(2 - 4)
-console.log(10 / 5)
-console.log(5 * 2)
-console.log(5 > 2)
-console.log(8 < 9)
-console.log(11 == 11)
-console.log(8 != 9)
-console.log(10 >= 10)
-console.log(10 <= 10)
+// Let's say you're tasked with coming up with some code that will check if the currentTime variable is between 9 a.m. and 5 p.m.
+// The code needs to console.log true if currentTime > 9 and if currentTime < 17.
+
+let currentTime = 10;
+console.log(currentTime > 9 && currentTime < 17);
+
+let gameScore = 40;
+console.log(gameScore > 30 && gameScore < 50);
+
+
+
+// Logical OR operator: || 
+
+// Logical NOT operator: ! 
+
+// The modulus operator: % 
+
+// The equality operator: ==
+
+// The strict equality operator: ===
+
+// The inequality operator: !=
+
+// The strict inequality operator: !==
+
+// The addition assignment operator: += 
+
+// The concatenation assignment operator: += (it's the same as the previous one - more on that later)
+
