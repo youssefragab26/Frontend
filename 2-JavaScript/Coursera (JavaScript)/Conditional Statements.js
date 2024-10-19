e6242df3af513c2da6924c406dc109778599db61
Youssefragab26 <youssef26ragab@gmail.com>
2024-10-19 22:32:33 +0300

    update

--- 2-JavaScript/Coursera (JavaScript)/Conditional Statements.js
+++ 2-JavaScript/Coursera (JavaScript)/Conditional Statements.js
@@ -77,3 +77,37 @@ switch (place) {
     default:
         console.log('No Medal');
 }
+
+// Example
+// Traffic Light
+
+var light = "green";
+
+if(light == "green") {
+    console.log("Drive")
+} else if (light == "orange") {
+    console.log("Get ready")
+} else if (light == "red") {
+    console.log("Dont' drive")
+} else {
+    //this block will run if no condition matches
+    console.log("The light is not green, orange, or red");
+}
+
+// on switch
+
+switch(light) {
+    case 'green':
+        console.log("Drive");
+        break;
+    case 'orange':
+        console.log("Get ready");
+        break;
+    case 'red':
+        console.log("Don't drive");
+        break;
+    default:
+        //this block will run if no condition matches
+        console.log('The light is not green, orange, or red');
+        break;
+ }
