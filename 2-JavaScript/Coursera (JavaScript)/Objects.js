6f0b2f4e2cf75eaf3e3b9fa961efed5eda26a54d
Youssefragab26 <youssef26ragab@gmail.com>
2024-10-22 17:44:43 +0300

    update

---
+++ 2-JavaScript/Coursera (JavaScript)/Objects.js
@@ -0,0 +1,26 @@
+var objectName = {} // create an object
+
+//creating an object with properties and their values
+
+var assistantManager = {
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40,
+    specialAbility: "Young and ambitious",
+    greeting: "Let's make some money"
+}
+
+// Example:
+
+let PrsonalInfo = {
+    name: "Youssef",
+    age: 21,
+    address: "Egypt",
+    phone: "01113985295",
+    email: "w9vZq@example.com",
+    gender: "male",
+}
+
+// to add new property to an object
+PrsonalInfo.job = "programmer";
